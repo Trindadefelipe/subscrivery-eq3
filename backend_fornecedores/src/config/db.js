98edcf1996d129615b469da4a98f054be7014dd8
Felipe Trindade <felipe_biel3@hotmail.com>
2025-12-26 14:17:29 -0300

    Update db.js
    FIX: SSL FINAL

--- backend_fornecedores/src/config/db.js
+++ backend_fornecedores/src/config/db.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 4000,
     ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: false
