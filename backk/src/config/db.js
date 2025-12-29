a541730ada59e3d5dc62ef4f7ec673d0dae2114a
Felipe Trindade <felipe_biel3@hotmail.com>
2025-12-29 20:10:15 -0300

    Update db.js para deploy

--- backk/src/config/db.js
+++ backk/src/config/db.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Adiciona suporte a arquivo .env localmente
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', 
-  password: '', 
-  database: 'subscrivery' 
+  host: process.env.DB_HOST,       // Lê o Host das variáveis
+  user: process.env.DB_USER,       // Lê o Usuário
+  password: process.env.DB_PASS,   // Lê a Senha
+  database: process.env.DB_NAME,   // Lê o Nome do Banco
+  port: process.env.DB_PORT || 4000, // Porta 4000 para TiDB
+  ssl: {
+    rejectUnauthorized: true       // <--- AQUI ESTÁ O CÓDIGO SSL QUE O TIDB EXIGE
+  }
 });
 
 db.connect((err) => {
