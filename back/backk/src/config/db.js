ee8835fcfed66122cafbec1f2a45a15ef6872482
Felipe Trindade <felipe_biel3@hotmail.com>
2025-12-29 21:35:00 -0300

    Fix: Configurando variaveis de ambiente e SSL

--- back/backk/src/config/db.js
+++ back/backk/src/config/db.js
@@ -1,10 +1,16 @@
+require('dotenv').config(); // Garante que funcione localmente também
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', 
-  password: '', 
-  database: 'subscrivery' 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS, // Usando DB_PASS conforme configurado na Vercel
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 4000, // Usa a porta 4000 do TiDB
+  ssl: {
+      minVersion: 'TLSv1.2',
+      rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
